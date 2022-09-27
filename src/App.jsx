6ab3b702703dc3891eec95fa1e01cf564f12f062
sabine <khun7706@gmail.com>
2022-09-27 13:29:07 +0800

    Add files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Typography } from "@mui/material";
+const App = () => {
+    return (
+        <div className='App'>
+            <Typography variant='h3'>Hello, This is my App</Typography>
+            <Button color='secondary' variant='contained'>
+                CLICK!!
+            </Button>
+        </div>
+    );
+};
+
+export default App;
